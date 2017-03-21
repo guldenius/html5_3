ea4353a5b9bdef114acafeed98b486a1de90f185
guldenius <guldenius@gmail.com>
2017-03-21 09:27:40 +0100

    project settings

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Element selector
+// @param selector: string element select
+function _q(selector){
+    //select elements
+    var elements = document.querySelectorAll(selector);
+    return elements;
+}
+
