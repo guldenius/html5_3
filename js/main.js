5b83d7c4c0d1ffb9471fafb4cada15cb3adb840c
guldenius <guldenius@gmail.com>
2017-03-24 10:52:54 +0100

    added animation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 // Element selector
 // @param selector: string element select
-function _q(selector){
+function _q(selector) {
     //select elements
     var elements = document.querySelectorAll(selector);
     return elements;
 }
 
+
+// input profile field display
+function showProfile() {
+
+    //select element
+    var div = document.querySelector(".profile-div");
+
+    // If contains the "show" than it will be disappear
+    if (div.classList.contains("show")) {
+        div.classList.remove("show");
+        console.log(div);
+    } else {
+        //Else add the "show"
+        div.classList.add("show");
+        console.log(div);
+    }
+
+}
